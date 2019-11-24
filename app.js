8a780d1632dd7b4ea9a9dd33e1374005a4fd32f0
Oscar Jimenez <osc9718@gmail.com>
2019-11-24 22:09:36 +0100

    Añadida conexión del cliente mongoose con la BD closes #45

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const expressGraphQL = require("express-graphql");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require('dotenv').config();
@@ -8,6 +9,7 @@ const schema = require("./graphql/");
 //Creamos la aplicación express
 const app = express();
 const PORT = process.env.PORT;
+const db = process.env.DB;
 
 app.use(
     "/graphql",
@@ -27,4 +29,14 @@ app.use(
   //Levantamos el servidor express en el puerto 4000
   app.listen(PORT|4000, () => {
     console.log(new Date().toString() + ": " + `Server running on port ${PORT}`);
+    // Conectar a MongoDB empleando el cliente Mongoose.
+    mongoose.connect(db,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    ).then(() => {
+      console.log(new Date().toString() + ": " + "MongoDB connected");
+    }).catch(err => console.log(err));
   });
