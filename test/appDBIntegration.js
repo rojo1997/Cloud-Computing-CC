8a73ab5f476eca5867a9865fb3a4a02b2f5349df
Oscar Jimenez <osc9718@gmail.com>
2019-11-28 23:37:39 +0100

    Añadido test de registro de producto con DB #48

--- test/appDBIntegration.js
+++ test/appDBIntegration.js
@@ -6,25 +6,37 @@ describe('Test DB Integration ', () => {
 
   describe('Should pass if the mutations are valid', () => {
     it('Valid expected query productByName', (done) => {
-      const mutation = {
+      const mutationPBN = {
         "query": "query productByName($name: String!) { productByName(name: $name){ name } }",
         "operationName": "productByName",
         "variables": "{ \"name\": \"Pechuga de pollo\" }"
       }
-      request(app).post('/graphql').send(mutation).end(function(err, res) { 
+      request(app).post('/graphql').send(mutationPBN).end(function(err, res) { 
         expect(res.body.data.productByName[0].name).to.equal('Pechuga de pollo'); 
         done(); 
       })
     })
 
     it('Valid expected query modifyProductState', (done) => {
-      const mutation = {
+      const mutationMPS = {
         "query": "mutation modifyProductState($_id: String!, $state: String!) { modifyProductState(_id: $_id, state: $state){ success } }",
         "operationName": "modifyProductState",
         "variables": "{ \"_id\": \"5ddda274409f381160c8452b\", \"state\": \"Disponible\" }"
       }
-      request(app).post('/graphql').send(mutation).end(function(err, res) { 
+      request(app).post('/graphql').send(mutationMPS).end(function(err, res) { 
         expect(res.body.data.modifyProductState.success).to.be.a('boolean'); 
+        done(); 
+      })
+    })
+
+    it('Valid expected query registerProduct', (done) => {
+      const mutationRP = {
+        "query": "mutation registerProduct($name: String!, $description: iDescription!, $state: String, $owner: String) { registerProduct(name: $name, description: $description, state: $state, owner: $owner){ success } }",
+        "operationName": "registerProduct",
+        "variables": "{ \"name\": \"Producto de test\", \"description\": {\"textDescription\": \"Producto de test\", \"price\": 5.0 , \"reducedPrice\": 4.0 , \"expiration\": \"Hoy\" }, \"state\": \"Testeado\", \"owner\": \"Tester\" }"
+      }
+      request(app).post('/graphql').send(mutationRP).end(function(err, res) { 
+        expect(res.body.data.registerProduct.success).to.be.a('boolean'); 
         done(); 
       })
     })
