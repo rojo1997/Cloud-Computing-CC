15f7f35e501a3e5ee45a1cd09023d02e836f9124
Oscar Jimenez <osc9718@gmail.com>
2019-12-04 12:42:53 +0100

    Reestructuración del repositorio #57

--- graphql/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const Product = require('./Product/');
-const mergeResolvers = require('merge-graphql-schemas').mergeResolvers;
-//En el caso de añadir nuevos resolvers para una nueva clase,
-//podremos establecer una organización de éstos, unificando 
-//su definición en lugar de tener los resolvers de cada clase 
-//en un mismo archivo.
-const resolversDefs = [Product];
-//Exportamos la definición de resolvers
-module.exports = mergeResolvers(resolversDefs, {all:true});
