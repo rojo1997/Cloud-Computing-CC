0cb6053061394d7de4d4cc3481f770d33f3d591b
Óscar Jiménez Fernández <osc9718@gmail.com>
2020-01-18 21:24:32 +0100

    Actualizado tasks.py para instalar dependencias #64

--- tasks.py
+++ tasks.py
@@ -7,7 +7,7 @@ def start(ctx, ms='all'):
     if(ms=='gp'):
         ctx.run("npm start")
     if(ms=='ar'):
-            ctx.run("gunicorn --chdir src app:api & python src/analyzer.py")
+            ctx.run("gunicorn -w 4 -b 0.0.0.0:8080 --chdir src app:api & python src/analyzer.py")
         
         
 
@@ -19,3 +19,13 @@ def test(ctx, ms='all'):
         ctx.run("npm test")
     if(ms=='ar'):
         ctx.run("coverage run -m unittest src/test/app_test.py")
+
+@task
+def install(ctx, ms='all'):
+    if(ms=='all'):
+        ctx.run("npm install --production & pip install requirements.txt")
+    if(ms=='gp'):
+        ctx.run("npm install --production")
+    if(ms=='ar'):
+        ctx.run("pip install -r requirements.txt")
+
