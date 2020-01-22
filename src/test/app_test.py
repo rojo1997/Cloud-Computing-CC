44060a04af9d6fe75f29550a72ecd20a8f6781c3
Óscar Jiménez Fernández <osc9718@gmail.com>
2020-01-22 21:16:47 +0100

    Actualizados test para comprobación de posts mal formados closes #62

--- src/test/app_test.py
+++ src/test/app_test.py
@@ -22,10 +22,12 @@ class TestApp(Tester):
         self.assertEqual(result.status, HTTP_200)
 
     #Función para testear la obtención de una receta atendiendo a su identificador
+    #5dffa8001348e798acaeff54
+    #5e20e60d6859cdc7b2517b60
     def test_get_productById(self):
-        result = self.simulate_get('/receipes?method=byId&value=5e20e60d6859cdc7b2517b60')
+        result = self.simulate_get('/receipes?method=byId&value=5dffa8001348e798acaeff54')
         self.assertEqual(result.status, HTTP_200)
-        self.assertEqual(result.json['name'], "Pollo con almendras")
+        self.assertEqual(result.json['name'], "Katsudon")
 
     #Función para testear la obtención de recetas atendiendo a sus ingredientes
     def test_get_productByIngredients(self):
@@ -39,12 +41,28 @@ class TestApp(Tester):
         self.assertEqual(result.status, HTTP_501)
 
     #Función para testear el recurso get con un valor de identificador de receta inexistente
+    #5e40e60d6859cdc7b2517b60
     def test_get_notFound(self):
         result = self.simulate_get('/receipes?method=byId&value=5e40e60d6859cdc7b2517b60')
         self.assertEqual(result.status, HTTP_400)
 
     #Función para testear el recurso post para la inserción de una nueva receta
     def test_post(self):
-        #testReceipe = dict(name='Receta de test', ingredients=['Ingrediente de test 1', 'Ingrediente de test 1'])
-        result = self.simulate_post('/receipes', json={"name": "Test", "ingredients":["ingrediente 1", "ingrediente 2", "ingrediente 3"]}, headers={'content-type': 'application/json'})
+        result = self.simulate_post('/receipes', json={"name": "Test", "ingredients":["ingrediente 1", 
+        "ingrediente 2", "ingrediente 3"]}, headers={'content-type': 'application/json'})
         self.assertEqual(result.status, HTTP_201)
+
+    #Función para testear el recurso post para una inserción mal formada
+    def test_post_badRequest_params(self):
+        #Introducimos unos parámetros incorrectos para la llamada al recurso 
+        #post definido para la inserción de una nueva receta
+        result = self.simulate_post('/receipes', json={"notName": "Test", "notIngredients":["ingrediente 1", 
+        "ingrediente 2", "ingrediente 3"]}, headers={'content-type': 'application/json'})
+        self.assertEqual(result.status, HTTP_400)
+
+    #Función para testear el recurso post para una inserción con un tipo incorrecto
+    def test_post_badRquest_type(self):
+        #Introducimos unos parámetros correctos pero de tipo incorrecto para la llamada al recurso 
+        #post definido para la inserción de una nueva receta
+        result = self.simulate_post('/receipes', json={"name": "Test", "ingredients":"Not an array of ingredients"}, headers={'content-type': 'application/json'})
+        self.assertEqual(result.status, HTTP_400)
